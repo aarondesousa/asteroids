05aae4350afd95a7fc08d2804482297ff2241e88
aarondesousa <aarondesousa1@gmail.com>
2025-03-29 20:58:07 +0530

    add player rotation on key press (A/D)

--- player.py
+++ player.py
@@ -1,6 +1,9 @@
 import pygame
 
-from constants import PLAYER_RADIUS
+from constants import (
+    PLAYER_RADIUS,
+    PLAYER_TURN_SPEED,
+)
 from circleshape import CircleShape
 
 
@@ -8,7 +11,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
- 
+
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
@@ -19,3 +22,13 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
